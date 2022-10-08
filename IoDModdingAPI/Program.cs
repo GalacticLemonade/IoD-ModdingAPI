57021737e4da4d8b62a62a5d5a1515463e097f7d
GalacticLemonade <aidenmoore1820@gmail.com>
2022-10-08 12:06:39 -0600

    commit

--- IoDModdingAPI/Program.cs
+++ IoDModdingAPI/Program.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace IoDModdingAPI
 {
     class Program
     {
-        static void Main(string[] args)
+        static string LogPathWithEnv = @"%USERPROFILE%\AppData\LocalLow\Radiangames\Instruments\Player.log";
+        static string RealFilePath = Environment.ExpandEnvironmentVariables(LogPathWithEnv);
+        static void PrepareConsole()
         {
-            Console.Title = "IoD Server";
-            Console.WriteLine("Now running IoD server! Please type 'exit' to exit.");
-            Console.ReadKey();
+            Console.Title = "IoD Modding API";
+            Console.WriteLine("Locating game...");
+
+            string[] lines = System.IO.File.ReadAllLines(RealFilePath);
+
+            string foundDir = lines[1].Substring(44, 73);
+
+            string finalDir = foundDir + "Instruments.exe";
+
+            Console.WriteLine("Game found! Directory: "+ finalDir);
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("Starting game...");
+
+            Process.Start(finalDir);
+            System.Threading.Thread.Sleep(1000000000);
+        }
+
+        static void Main()
+        {
+            PrepareConsole();
         }
     }
 }
