b5a0589d42cb0e9debb101ffeea2d380a7bbd1f6
GalacticLemonade <aidenmoore1820@gmail.com>
2022-10-07 20:03:52 -0600

    Add project files.

---
+++ IoDModdingAPI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoDModdingAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "IoD Server";
+            Console.WriteLine("Now running IoD server! Please type 'exit' to exit.");
+            Console.ReadKey();
+        }
+    }
+}
